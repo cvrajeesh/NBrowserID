fbc70be88368fd19d64e7c9f5a48a16858d50156
cvrajeesh <cvrajeesh@gmail.com>
2011-12-21 22:37:39 +0530

    Nuget package

--- NBrowserID/Properties/AssemblyInfo.cs
+++ NBrowserID/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NBrowserID")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for BrowserID based authentication")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NBrowserID")]
-[assembly: AssemblyCopyright("Copyright © HP 2011")]
+[assembly: AssemblyCopyright("Copyright © Rajeesh C V 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ README.md
@@ -0,0 +1,13 @@
+NBrowserID
+===========
+
+A Library which helps you to integrate [BrowserID](https://browserid.org) into your Web Application.
+
+Go through Demo application to see how it is done
+
+Rewerences
+----------
+*[Integrating BrowserID In To Your ASP.NET MVC Application](http://www.rajeeshcv.com/post/details/46/integrating-browserid-in-to-your-asp-net-mvc-application)
+
+
+
